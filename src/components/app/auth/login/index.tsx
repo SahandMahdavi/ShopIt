f99297b8be3364d95106c4ef5432966d8897e90e
Sahand <shndmahdavi@gmail.com>
2021-06-17 02:06:41 +0430

    Added Configs for: Parse, Redux, App Router

---
+++ src/components/app/auth/login/index.tsx
@@ -0,0 +1,12 @@
+/**
+ * Created By: Sahand Mahdavi
+ * Email: shndmahdavi@gmail.com
+ */
+
+export function Login() {
+  return (
+    <div id="authenticate">
+      <p>Login</p>
+    </div>
+  );
+}
